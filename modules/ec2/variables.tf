7f399e4eb7b9ca14153c4c53c2427225dcf1b34b
Varun Nalluri <108406501+varun0170@users.noreply.github.com>
2025-12-26 02:05:28 +0000

    Add EC2 instance type and subnet ID variables

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,1 +1,11 @@
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID to launch EC2 in"
+  type        = string
+}
 
