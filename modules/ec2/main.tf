f51ab4d8ea1cac3d5d7f237f594d460413c7a73c
Varun Nalluri <108406501+varun0170@users.noreply.github.com>
2025-12-26 02:04:28 +0000

    Add AWS EC2 instance resource configuration

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,11 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316"  # Ubuntu 22.04 LTS in eu-west-2
+  instance_type = var.ec2_instance_type
+  subnet_id     = var.subnet_id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "varun-ec2-web"
+  }
+}
 
