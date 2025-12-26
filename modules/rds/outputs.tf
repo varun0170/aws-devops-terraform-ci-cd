ceeb617cffb775e49248c9befe90711805cdb8ec
Varun Nalluri <108406501+varun0170@users.noreply.github.com>
2025-12-26 02:11:37 +0000

    Add outputs for RDS endpoint and instance ID

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,1 +1,10 @@
 
+output "rds_endpoint" {
+  description = "RDS endpoint"
+  value       = aws_db_instance.this.endpoint
+}
+
+output "rds_id" {
+  description = "RDS instance ID"
+  value       = aws_db_instance.this.id
+}
