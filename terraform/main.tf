f15cbf744dab87aa7e6ff85a9299cb20495c9b29
Varun Nalluri <108406501+varun0170@users.noreply.github.com>
2025-12-26 02:03:03 +0000

    Add AWS provider and VPC module configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,13 @@
 
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source        = "../modules/vpc"
+  vpc_cidr      = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+}
