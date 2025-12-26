2c049034b87304ad55bd5616cfdff2bdf1b6b57e
Varun Nalluri <108406501+varun0170@users.noreply.github.com>
2025-12-26 01:58:35 +0000

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,30 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "varun-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = "varun-public-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = "varun-private-${count.index + 1}"
+  }
+}
 
