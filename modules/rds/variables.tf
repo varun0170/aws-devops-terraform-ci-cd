da76099d1948e48d7332f844be5db2d200931f9c
Varun Nalluri <108406501+varun0170@users.noreply.github.com>
2025-12-26 02:10:56 +0000

    Add RDS variables for configuration

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,1 +1,35 @@
+variable "rds_instance_type" {
+  description = "RDS instance type"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "private_subnets" {
+  description = "Private subnet IDs for RDS"
+  type        = list(string)
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "mydb"
+}
+
+variable "db_username" {
+  description = "Master username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "Master password"
+  type        = string
+  default     = "Password123!"
+}
+
+variable "security_group_ids" {
+  description = "Security groups for RDS"
+  type        = list(string)
+  default     = []
+}
 
